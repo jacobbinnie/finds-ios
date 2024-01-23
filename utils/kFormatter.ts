b85276b2deebfa7389c35a6fa0254a74b61a59a3
jacobbinnie <hello@jacobbinnie.com>
2024-01-23 15:41:28 -0500

    feat: improve kFormatter

--- utils/kFormatter.ts
+++ utils/kFormatter.ts
@@ -1,5 +1,9 @@
 export function kFormatter(num: number) {
-  return Math.abs(num) > 999
-    ? (Math.sign(num) * (Math.abs(num) / 1000)).toFixed(1) + "k"
-    : Math.sign(num) * Math.abs(num);
+  if (Math.abs(num) >= 1e6) {
+    return (Math.sign(num) * (Math.abs(num) / 1e6)).toFixed(1) + "M";
+  } else if (Math.abs(num) > 999) {
+    return (Math.sign(num) * (Math.abs(num) / 1000)).toFixed(1) + "k";
+  } else {
+    return Math.sign(num) * Math.abs(num);
+  }
 }
