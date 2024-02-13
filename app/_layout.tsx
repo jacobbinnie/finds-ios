b4fa0f8ab79d066daa6ce1197e568a32e77baeec
jacobbinnie <hello@jacobbinnie.com>
2024-02-13 11:32:05 -0500

    feat: set default route to login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,7 +14,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+  initialRouteName: "(modals)/login",
 };
 
 // SplashScreen.preventAutoHideAsync();
